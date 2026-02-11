48c496d52644c2827ebb94ede8572cef74922d01
Igor Kosalapov <ikosalapov@yandex.ru>
2026-02-11 13:58:39 +0500

    Исправление замечаний Checkstyle

--- ewm-service/ewm-server/src/main/java/ru/practicum/ewm/server/event/dto/UpdateEventAdminRequest.java
+++ ewm-service/ewm-server/src/main/java/ru/practicum/ewm/server/event/dto/UpdateEventAdminRequest.java
@@ -1,7 +1,6 @@
 package ru.practicum.ewm.server.event.dto;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
